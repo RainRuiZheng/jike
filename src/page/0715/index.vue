639065226bb1f985cd10ee7f76e2033535f8c514
寻迹 <oncwnuMXIa0s4LDFNvFKkti16SQY@git.weixin.qq.com>
2021-07-21 21:08:58 +0800

    feat:0715

--- src/page/0715/index.vue
+++ src/page/0715/index.vue
@@ -1,23 +1,19 @@
 <!--
  * @Author: your name
  * @Date: 2021-07-21 10:37:14
- * @LastEditTime: 2021-07-21 18:07:16
+ * @LastEditTime: 2021-07-21 21:00:26
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: /online-advisory/Users/zhengrui/Desktop/jike/src/page/0715/index.vue
+ 作业 请在以下 2个题目中任选一题解答： 
+ 1、链表反转 2、数组中第 K 大的元素
 -->
-/* * @Author: your name * @Date: 2021-07-21 10:37:14 * @LastEditTime: 2021-07-21
-10:37:15 * @LastEditors: Please set LastEditors * @Description: 作业 请在以下 2
-个题目中任选一题解答： 1、链表反转 2、数组中第 K 大的元素 提交链接：
-https://jinshuju.net/f/IQnNT9 * @FilePath:
-/online-advisory/Users/zhengrui/Desktop/jike/src/page/0715/index.js */
 <template>
   <div class="kth">
     <section class="show-area">
       <span>数组：{{ arr }}</span>
-    <!--<span>有序数组：{{arr.sort(function(a,b){return b-a})}}</span>-->
+      <!--<span>有序数组：{{arr.sort(function(a,b){return b-a})}}</span>-->
       <span class="item">
-        寻找数组中第 K 大的元素:<input v-model="k" min="1" :max="arr.length" />
+        寻找数组中第 K 大的元素:<input v-model="k" min="1" :max="arr.length" type="range"/>{{this.k}}
       </span>
       <span class="item">
         <button @click="findKth">计算</button>
@@ -34,7 +30,7 @@ export default {
     let arr = [7, 5, 15, 3, 17, 2, 20, 24, 1, 9, 12, 8];
     return {
       arr,
-      k: 0,
+      k: 1,
       kth: "",
       aLen: arr.length,
     };
@@ -42,47 +38,42 @@ export default {
   methods: {
     findKth() {
       let mid = this.quickSort(this.arr, 0, this.aLen - 1);
-      if (mid > this.aLen - this.k) {
-        mid = this.quickSort(this.arr, 0, mid - 1);
-      } else if (mid < this.aLen - this.k) {
-        mid = this.quickSort(this.arr, mid + 1, this.aLen - 1);
+      while (mid != this.aLen - this.k) {
+        if (mid > this.aLen - this.k) {
+          mid = this.quickSort(this.arr, 0, mid - 1);
+        } else if (mid < this.aLen - this.k) {
+          mid = this.quickSort(this.arr, mid + 1, this.aLen - 1);
+        }
       }
-
-      this.kth = this.arr[mid];
+      this.kth = this.arr[mid]; 
     },
-    quickSort( left, right) {
-      if (!this.arr) {
-        return -1;
-      }
+    quickSort(arr, left, right) {
       if (left > right) {
         return -1;
       }
 
       let low = left;
       let high = right;
-
-      // 选择基准数
-      let temp = this.arr[left];
+      let temp = arr[left];
       while (low < high) {
-        while (low < high && this.arr[high] >= temp) {
+        while (low < high && arr[high] >= temp) {
           high--;
         }
 
-        while (low < high && this.arr[low] <= temp) {
+        while (low < high && arr[low] <= temp) {
           low++;
         }
-
+        console.log('while',low,high,arr)
         if (low < high) {
-          let t = this.arr[low];
-          this.arr[low] = this.arr[high];
-          this.arr[high] = t;
+          let t = arr[low];
+          arr[low] = arr[high];
+          arr[high] = t;
         }
       }
 
-      // 到这里说明low==high, 交换基准数和相遇点的位置
-
-      this.arr[left] = this.arr[low];
-      this.arr[low] = temp;
+      arr[left] = arr[low];
+      arr[low] = temp;
+      console.log('return',low)
       return low;
     },
   },
@@ -94,9 +85,9 @@ export default {
   display: flex;
   flex-direction: column;
   padding: 18px;
-  .show-area{
-      display: flex;
-      flex-direction: column;
+  .show-area {
+    display: flex;
+    flex-direction: column;
   }
   .item {
     padding-right: 10px;
