24d4e1d8a1e507e827cac9cf225b9351d4dc9c70
寻迹 <oncwnuMXIa0s4LDFNvFKkti16SQY@git.weixin.qq.com>
2021-07-21 23:22:18 +0800

    0717

--- src/page/0717/index.vue
+++ src/page/0717/index.vue
@@ -1,8 +1,54 @@
 <!--
  * @Author: your name
  * @Date: 2021-07-21 18:10:18
- * @LastEditTime: 2021-07-21 18:10:18
+ * @LastEditTime: 2021-07-21 22:42:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
+ let tmpl = `<div class="newslist">
+    <div class="img" v-if="info.showImage"><img src="{{image}}"/></div>
+    <div class="date" v-if="info.showDate">{{info.name}}</div>
+    <div class="img">{{info.name}}</div>
+</div>`;
+
+render(tmpl, {
+	image: "some img", 
+    info: {showImage: true, showDate:false, name: "aaa"}
+})
  * @FilePath: /online-advisory/Users/zhengrui/Desktop/jike/src/page/0717/index.vue
 -->
+
+<template>
+  <div class="tmpl"></div>
+</template>
+
+<script>
+import Engine from "./index";
+export default {
+  name: "Tmpl",
+  
+  mounted(){
+let tmpl = `<div class="newslist">
+                    <div class="img" v-if="info.showImage"><img src="{{image}}"/></div>
+                    <div class="date" v-if="info.showDate">{{info.name}}</div>
+                    <div class="img">{{info.name}}</div>
+                </div>`;
+
+    let data = {
+      image: require('./gift.png'),
+      info: { showImage: true, showDate: false, name: "aaa" },
+    };
+     const engine = new Engine();
+      let dom = engine.render(tmpl, data);
+      console.log(dom);
+      document.querySelector(".tmpl").appendChild(dom);
+  },
+};
+</script>
+
+<style lang="less">
+.tmpl {
+  display: flex;
+  flex-direction: column;
+  padding: 18px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-13 13:50:27
- * @LastEditTime: 2021-07-21 17:49:06
+ * @LastEditTime: 2021-07-21 21:15:05
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /online-advisory/Users/zhengrui/Desktop/jike/src/router/index.js
@@ -26,6 +26,11 @@ const routes = [
     path: "/kth",
     name: "Kth",
     component: ()=>import(/* webpackChunkName: "Kth" */ '../page/0715'),
+  },
+  {
+    path: "/tmpl",
+    name: "Tmpl",
+    component: ()=>import(/* webpackChunkName: "Tmpl" */ '../page/0717'),
   },
 ];
 
